9924d0f4931fe24cd7c73147033d3975a1ff60d2
Jose Daniel <jdrh97@gmail.com>
2019-01-11 22:29:53 -0600

    Added toast in the login

--- juncos-wa/src/app/users/login/login.component.ts
+++ juncos-wa/src/app/users/login/login.component.ts
@@ -3,6 +3,9 @@ import { AngularFireAuth } from '@angular/fire/auth';
 import { auth } from 'firebase/app';
 import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service'
+import { NgForm } from '@angular/forms';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-login',
@@ -11,9 +14,12 @@ import { AuthService } from '../../services/auth.service'
 })
 export class LoginComponent implements OnInit {
 
-  constructor(public afAuth: AngularFireAuth, private router: Router, private authService: AuthService) { }
+  constructor(public afAuth: AngularFireAuth, private router: Router, private authService: AuthService, private toastr: ToastrService) { }
   public email: string = '';
   public password: string = '';
+  public isInvalidUser: boolean = false;
+  public isInvalidPassword: boolean = false;
+
   ngOnInit() {
   }
 
@@ -21,7 +27,12 @@ export class LoginComponent implements OnInit {
     this.authService.loginEmailUser(this.email, this.password)
       .then((res) => {
         this.onLoginRedirect();
-      }).catch(err => console.log('err', err.message));
+        this.toastr.success('Inició secion', 'Bienvenido');
+      }).catch(err => {console.log('err', err.message); 
+        this.toastr.warning('Credenciales invalidos','Error');
+        this.isInvalidUser = true;
+        this.isInvalidPassword = true
+      });
   }
 
   onLoginGoogle(): void {
@@ -30,6 +41,27 @@ export class LoginComponent implements OnInit {
         this.onLoginRedirect();
       }).catch(err => console.log('err', err.message));
   }
+
+//   resetForm(form?: NgForm) {
+//     if (form != null)
+//       form.resetForm();
+//     this.service.formData = {
+//       id: null,
+//       username: '',
+//       password: '',
+//     }
+//   }
+
+//   onSubmit(form: NgForm) {
+//     let data = Object.assign({}, form.value);
+//     delete data.id;
+//     if (form.value.id == null)
+//       this.firestore.collection('users').add(data);
+//     else
+//       this.firestore.doc('users/' + form.value.id).update(data);
+//     this.resetForm(form);
+//     this.toastr.success('Admin registrado exitosamente', 'Registrar Admin');
+// }
 
   onLogout() {
     this.authService.logoutUser();
