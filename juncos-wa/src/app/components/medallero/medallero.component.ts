03902e747c409b4b56d889f81c65702fc91e6c7e
josue <david.rod13>
2019-01-20 13:48:34 -0800

    Insert and validate information about  a new fielld in DB

--- juncos-wa/src/app/components/medallero/medallero.component.ts
+++ juncos-wa/src/app/components/medallero/medallero.component.ts
@@ -1,7 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import {MatDialog} from '@angular/material/dialog';
-import {displayedColumns as dc,storedColumns as sc} from '../../interfaces/MedalleroInterface'
+import {MedalleroInterface,displayedColumns as dc,storedColumns as sc} from '../../interfaces/MedalleroInterface'
 import{dialogForm} from '../dialogs/dialogForm'
+import { ToastrService } from 'ngx-toastr';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Router } from '@angular/router';
+import { GetCollections } from '../../services/getCollections.service';
+
 @Component({
   selector: 'app-medallero',
   templateUrl: './medallero.component.html',
@@ -11,11 +16,11 @@ export class MedalleroComponent {
 
   storedColumns=sc;
   displayedColumns=dc;
-
+  private identificadores= []; 
   list:string[];
 
-  constructor(public dialog: MatDialog) {}
-
+  constructor(private service :GetCollections, public dialog: MatDialog, private router: Router,private firestore: AngularFirestore ,private toastr: ToastrService) {
+  }
   openDialog(): void {
     const dialogRef = this.dialog.open(dialogForm, {
       width: '400px',
@@ -24,10 +29,38 @@ export class MedalleroComponent {
     });
 
     dialogRef.afterClosed().subscribe(result => {
-      console.log('The dialog was closed');
-      console.log(result)
+      
       this.list = result;
-      //hay que agarrar list, validar los datos e insertarlos a la base de datos
+      var  csvRecord= <MedalleroInterface>{}
+     
+      this.identificadores = new Array();
+      console.log("Tomando informacion en base");
+      var ids = this.service.getMedalleroMod();
+      var allIds = ids.get().subscribe(snapshot => {
+        snapshot.forEach(doc => {
+          var x = doc.data();
+          this.identificadores.push(x[this.storedColumns[0]]);
+
+        });
+        if(this.identificadores.find(x => x === this.list[0]) ){
+          console.log("Elemento repetido"+ this.list[0]);
+          this.toastr.error("No fue posible agregar registro", "Datos invalido en identificador");
+        }
+        else{
+          for (let j = 0; j < this.list.length; j++) { // cols
+            csvRecord[this.storedColumns[j]] = this.list[j];
+          
+          }
+          var data = JSON.parse(JSON.stringify(csvRecord));
+          this.firestore.collection('medallero').add(data);
+          this.toastr.success("Registro agregado exitosamente", "Aceptar");
+          this.router.navigate(['medallero']);
+  
+        }
+        
+
+      }) 
+
     });
 
   }
